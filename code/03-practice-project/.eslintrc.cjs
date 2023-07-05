fbafa5895ab7bda158393acb040349ec7146de61
Lohit <jethwanilohit@gmail.com>
2023-07-05 05:33:34 +0530

    Edited Lint config

--- code/03-practice-project/.eslintrc.cjs
+++ code/03-practice-project/.eslintrc.cjs
@@ -1,32 +1,26 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true  // add this line to fix "'describe' is not defined" and similar errors
     },
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended"
     ],
     "parserOptions": {
-        "ecmaVersion": 2023,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    },
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+        "ecmaVersion": 2022,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
         }
-    ]
-}
+    },
+    "rules": {
+        "react/react-in-jsx-scope": "off"  // add this line to fix "'React' must be in scope when using JSX" error
+    },
+    "settings": {
+        "react": {
+            "version": "detect"  // add this line to fix "React version not specified in eslint-plugin-react settings" warning
+        }
+    }
+};
